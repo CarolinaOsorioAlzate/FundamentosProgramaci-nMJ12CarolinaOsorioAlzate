2bc4d8b4737ecbc4722ee879c899c40dff26e42c
CarolinaOsorioAlzate <carolina.osorioa@upb.edu.co>
2026-07-28 13:35:04 -0500

    Mi primer commit

---
+++ 1.VariablesConstantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1.VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí escribo el código fuente
+            string nombre="Carolina Osorio";
+            Console.WriteLine("Bienvenida " + nombre + " al curso Fundamentos de Programación");
+        }
+    }
+}
